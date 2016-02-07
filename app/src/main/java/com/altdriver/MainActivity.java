48614eee7dd900ac2e65a3e5b9f12374dfa60680
Christiaan Mealey <chris.mealey@gmail.com>
2016-02-06 22:36:50 -0500

    initial commit

---
+++ app/src/main/java/com/altdriver/MainActivity.java
@@ -0,0 +1,31 @@
+package com.altdriver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+        webView.loadUrl("http://www.altdriver.com");
+    }
+}
